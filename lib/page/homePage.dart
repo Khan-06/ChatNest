3c2e75bb6d2c759c53108af23dca8fdab2e017b3
Khan-06 <m.abdulbasit.k.06@gmail.com>
2024-02-18 01:17:50 +0500

    fixing an error!

--- lib/page/homePage.dart
+++ lib/page/homePage.dart
@@ -20,7 +20,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Center(child: Text('Home Page')), actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.logout))],),
+      appBar: AppBar(title: const Center(child: Text('Home Page')), actions: [IconButton(onPressed: signOut, icon: const Icon(Icons.logout))],),
     );
   }
 }
